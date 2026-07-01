cda07eae2252ac5714c854fd38f47dd4dd572577
komal jasapara <komaljasapara7878@gmail.com>
2026-07-01 14:34:56 +0530

    Update characterr.js

--- characterr.js
+++ characterr.js
@@ -1,6 +1,6 @@
 /*Write a JavaScript function reverseString that takes a 
 string as input and returns the string with its characters reversed.*/
-
+CODE TO WRITE
 function reverseString(str){
     let rev= "";
     for(let i=str.length-1; i>=0;i--)
